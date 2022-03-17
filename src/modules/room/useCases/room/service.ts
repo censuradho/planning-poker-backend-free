7f437bd706b559b1324c84c5e30d352e8ea70c29
Censuradho <gustavoleiteoliveira800@gmail.com>
2022-03-17 09:43:02 -0300

    retrive existent participant when already register an with same username

--- src/modules/room/useCases/room/service.ts
+++ src/modules/room/useCases/room/service.ts
@@ -5,32 +5,62 @@ import { prisma } from 'lib/prisma';
 import pino from 'lib/pino';
 
 import { CreateRoom } from 'websocket/handlers/roomHandler/types';
+import { Participant, Room } from '@prisma/client';
 
 export class RoomService {
   async createRoom ({ room_name, username, socket_id }: CreateRoom) {
     const result = await prisma.$transaction(async _prisma => {
-      const room = await _prisma.room.create({ 
-        data: {
-          id: uuid(),
+      const room = {} as Room
+      const participant = {} as Participant
+
+      const existRoom = await _prisma.room.findFirst({
+        where: {
           name: room_name
-        },
-        include: {
-          participants: true
         }
       })
 
-       const participant = await _prisma.participant.create({
-         data: {
-           id: socket_id,
-           username,
-           vote: '',
-           room: {
-             connect: {
-               id: room.id
-             }
-           }
-         }
-       })
+      if (existRoom) {
+        Object.assign(room, existRoom)
+      } else {
+        const newRoom = await _prisma.room.create({ 
+          data: {
+            id: uuid(),
+            name: room_name
+          },
+          include: {
+            participants: true
+          }
+        })
+        Object.assign(room, newRoom)
+      }
+
+      const existParticipant = await _prisma.participant.findFirst({
+        where: {
+          id: socket_id
+        }
+      })
+
+      if (existParticipant) {
+        Object.assign(participant, existParticipant)
+      } else {
+        const newParticipant = await _prisma.participant.create({
+          data: {
+            id: socket_id,
+            username,
+            isAdmin: true,
+            vote: '',
+            room: {
+              connect: {
+                id: room.id
+              }
+            }
+          }
+        })
+
+        Object.assign(participant, newParticipant)
+      }
+
+
 
        return {
          room,
